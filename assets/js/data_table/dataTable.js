8cd2bc26cf8f3fbfac1fd8b7d383a071f2b50c6a
Jahid <jahidhasan.developer@gmail.com>
2023-03-29 12:09:06 +0700

    update side nav & dataTable

---
+++ assets/js/data_table/dataTable.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     const prev = document.getElementById('example_previous');
+//     const nxt = document.getElementById('example_next');
+//     // console.log(prev)
+//     prev.innerText = '‹'
+//     nxt.innerText = '›'
+// });
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,89 +16,91 @@ function controlLeftMenu() {
 }
 
 // toggle dashboard card
-function toggleCard(id) {
+function toggleCard(id, iconId) {
   $(document).ready(function () {
+    if (iconId) {
+      const icon = document.getElementById(iconId);
+      if ($(`#${id}`).css("display") == "none") {
+        icon.classList.remove("active");
+      } else {
+        icon.classList.add("active");
+      }
+    }
     $(`#${id}`).slideToggle();
   });
 }
+
 $("#myModal").on("shown.bs.modal", function () {
   $("#myInput").trigger("focus");
 });
 
-
-
 // input validation
-function inputValidator(e, warnText, type = "text", length=6 ) {
-  const inputValue = e
+function inputValidator(e, warnText, type = "text", length = 6) {
+  const inputValue = e;
   const warnTextContent = document.getElementById(warnText);
-  if (type === 'text') {
-    if(inputValue.length < 1){
+  if (type === "text") {
+    if (inputValue.length < 1) {
       warnTextContent.classList.add("active");
-    }else{
-    warnTextContent.classList.remove('active')
+    } else {
+      warnTextContent.classList.remove("active");
+    }
   }
-  }
-  if(type === "password"){
-    if(inputValue.length < length){
-      if(inputValue.length === 0){
-        warnTextContent.classList.add('active');
-      }else{
-        warnTextContent.classList.add('active');
-      warnTextContent.innerText = `Password must be ${length} character`
+  if (type === "password") {
+    if (inputValue.length < length) {
+      if (inputValue.length === 0) {
+        warnTextContent.classList.add("active");
+      } else {
+        warnTextContent.classList.add("active");
+        warnTextContent.innerText = `Password must be ${length} character`;
       }
-      
-    }else{
-    warnTextContent.classList.remove('active')
-  }
+    } else {
+      warnTextContent.classList.remove("active");
+    }
   }
 }
-function showPass(id){
+function showPass(id) {
   const input = document.getElementById(id);
-  if(input.type === 'password'){
-    input.type = 'text'
-  }else{
-    input.type = 'password'
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
   }
 }
 
-function filterOption(){
+function filterOption() {
   const filter_content = document.getElementById("filter_content");
   filter_content.classList.toggle("active");
 }
 
-
-
-function removeTag(items){
-  const filteredItems = tags.filter((item) =>{
-   return item !== items
-  })
-  tags = filteredItems
-  renderTag()
-
+function removeTag(items) {
+  const filteredItems = tags.filter((item) => {
+    return item !== items;
+  });
+  tags = filteredItems;
+  renderTag();
 }
 
 // img preview
 
-function PreviewImage(selectFile,previewImg) {
+function PreviewImage(selectFile, previewImg) {
   var oFReader = new FileReader();
   oFReader.readAsDataURL(document.getElementById(selectFile).files[0]);
 
   oFReader.onload = function (oFREvent) {
-      document.getElementById(previewImg).src = oFREvent.target.result;
+    document.getElementById(previewImg).src = oFREvent.target.result;
   };
-};
+}
 
- function cancelPreview(id){
+function cancelPreview(id) {
   const img = document.getElementById(id);
-  img.src = '../assets/images/img_preview.png'
- }
+  img.src = "../assets/images/img_preview.png";
+}
 
 // remove class
 document.onclick = function (e) {
   const profile = document.getElementById("profile_content");
-  if (profile){
+  if (profile) {
     if (
-    
       e.target.parentNode.id !== "profile_pic" &&
       e.target.parentNode.id !== "profile_content" &&
       e.target.parentNode.parentNode.id !== "profile_content"
